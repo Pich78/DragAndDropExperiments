2269db71920d694086981d138a798bce41eb247c
Pich78 <apiccianoit@yahoo.it>
2019-09-20 01:14:06 +0200

    Added JFoenix material design library

--- src/sample/DraggableButton.java
+++ src/sample/DraggableButton.java
@@ -1,12 +1,13 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 import java.beans.EventHandler;
 
-public class DraggableButton extends Button {
+public class DraggableButton extends JFXButton {
 
     public DraggableButton (String name) {
 
@@ -20,11 +21,15 @@ public class DraggableButton extends Button {
     }
 
     void MousePressedAction(MouseEvent e) {
+        // Make sure the node is not picked
+        //setMouseTransparent(true);
         print("Source: " + ((Button)e.getSource()).getText() + " pressed");
         // Generate drag detect event after the current mouse pressed event
         e.setDragDetect(true);
     }
     void MouseDraggedAction(MouseEvent e) {
+        // Start a full press-drag-release gesture
+        //startFullDrag();
         print("Source: " + ((Button)e.getSource()).getText() + " dragged");
         // Suppress the drag detected default event generation after mouse dragged
         e.setDragDetect(false);
@@ -33,6 +38,8 @@ public class DraggableButton extends Button {
         print("Source: " + ((Button)e.getSource()).getText() + " drag detected");
     }
     void MouseReleasedAction(MouseEvent e) {
+        // Make sure the node is picked
+        setMouseTransparent(false);
         print("Source: " + ((Button)e.getSource()).getText() + " released");
     }
 
