1da8dbc7a248702410e7d425728b36fdfeaf3b2d
Pich78 <apiccianoit@yahoo.it>
2019-09-20 08:15:31 +0200

    Printed button and mouse position

--- src/sample/DraggableButton.java
+++ src/sample/DraggableButton.java
@@ -24,6 +24,10 @@ public class DraggableButton extends JFXButton {
         // Make sure the node is not picked
         //setMouseTransparent(true);
         print("Source: " + ((Button)e.getSource()).getText() + " pressed");
+        print("Mouse x: "+ e.getX());
+        print("Mouse y: "+ e.getY());
+        print("Button x: " + ((Button)e.getSource()).getLayoutX());
+        print("Button y: " + ((Button)e.getSource()).getLayoutY());
         // Generate drag detect event after the current mouse pressed event
         e.setDragDetect(true);
     }
@@ -31,16 +35,22 @@ public class DraggableButton extends JFXButton {
         // Start a full press-drag-release gesture
         //startFullDrag();
         print("Source: " + ((Button)e.getSource()).getText() + " dragged");
+        //print("x: "+ e.getX());
+        //print("y: "+ e.getY());
         // Suppress the drag detected default event generation after mouse dragged
         e.setDragDetect(false);
     }
     void MouseDragDetectedAction(MouseEvent e) {
         print("Source: " + ((Button)e.getSource()).getText() + " drag detected");
+        //print("x: "+ e.getX());
+        //print("y: "+ e.getY());
     }
     void MouseReleasedAction(MouseEvent e) {
         // Make sure the node is picked
         setMouseTransparent(false);
         print("Source: " + ((Button)e.getSource()).getText() + " released");
+        //print("x: "+ e.getX());
+        //print("y: "+ e.getY());
     }
 
     private void print(String msg) {
